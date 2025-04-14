719b121ee48e03fb3a325c2e7df598c8109d2787
eliewebavenue <elie@webavenue.agency>
2025-04-14 16:25:12 +0300

    add link to css theme

--- header.php
+++ header.php
@@ -5,11 +5,12 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo get_bloginfo('name'); ?></title>
-	<link href="css/bootstrap.min.css" rel="stylesheet" >
-	<link href="css/font-awesome.min.css" rel="stylesheet" >
-	<link href="css/global.css" rel="stylesheet">
-	<link href="css/index.css" rel="stylesheet">
+	<link href="<?php echo get_bloginfo('stylesheet_directory')  ?>/css/bootstrap.min.css" rel="stylesheet" >
+	<link href="<?php echo get_bloginfo('stylesheet_directory')  ?>/css/font-awesome.min.css" rel="stylesheet" >
+	<link href="<?php echo get_bloginfo('stylesheet_directory')  ?>/css/global.css" rel="stylesheet">
+	<link href="<?php echo get_bloginfo('stylesheet_directory')  ?>/css/index.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Rajdhani&display=swap" rel="stylesheet">
-	<script src="js/bootstrap.bundle.min.js"></script>
+	<script src="<?php echo get_bloginfo('stylesheet_directory')  ?>/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+	<h1><?php echo get_bloginfo('name'); ?></h1>
